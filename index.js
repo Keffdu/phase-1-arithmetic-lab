a95461786c5dbcd4ad3c85e14a80f34710e5ac6f
Kevin Dutzy <kddutz15@gmail.com>
2022-10-31 10:29:18 -0400

    commit tree 526cad642deec14c718b0958f8130dbc6bc9851f

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 31;
+const num2 = 2;
 
-// Write your code here
+let multiply = `${num1}` * `${num2}`;
+
+let random = Math.floor(Math.random() + 2);
+
+const num3 = 8
+const num4 = 2
+
+let mod = `${num3}` / `${num4}`;
+
+let max = Math.max(5, 10, 15, 20);
